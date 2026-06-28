9f01b40e58e3da9a9f03db2b0347e0412ccec9e7
Davide-Cruciani <davidecruciani0601@gmail.com>
2026-06-28 18:27:17 +0200

    Added NPR

---
+++ models/NPRDeepfakeDetection/__init__.py
@@ -0,0 +1,3 @@
+from ...project.models.NPRDeepfakeDetection import options
+from ...project.models.NPRDeepfakeDetection import networks
+from ...project.models.NPRDeepfakeDetection import validate
---
+++ models/NPRDeepfakeDetection/networks/__init__.py
@@ -0,0 +1,3 @@
+from ....project.models.NPRDeepfakeDetection.networks import resnet
+from ....project.models.NPRDeepfakeDetection.networks import base_model
+from ....project.models.NPRDeepfakeDetection.networks import trainer
