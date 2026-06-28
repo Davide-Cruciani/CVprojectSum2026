449e902a36e009c6a2b03654c4bf939f5c25ac53
Davide-Cruciani <davidecruciani0601@gmail.com>
2026-06-28 18:28:13 +0200

    Added UFD

---
+++ models/UniversalFakeDetect/__init__.py
@@ -0,0 +1,7 @@
+from ...project.models.UniversalFakeDetect import options
+from ...project.models.UniversalFakeDetect import data
+from ...project.models.UniversalFakeDetect import dataset_paths
+from ...project.models.UniversalFakeDetect import models
+from ...project.models.UniversalFakeDetect import networks
+from ...project.models.UniversalFakeDetect import validate
+# from . import train
