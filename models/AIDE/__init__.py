50e17c8aabf0304f8b17f53f95e9fe3a78df15af
Davide-Cruciani <davidecruciani0601@gmail.com>
2026-06-28 18:34:24 +0200

    Added AIDE

---
+++ models/AIDE/__init__.py
@@ -0,0 +1,2 @@
+from ...project.models.AIDE import models
+from ...project.models.AIDE import utils
---
+++ models/AIDE/models/__init__.py
@@ -0,0 +1,3 @@
+from ....project.models.AIDE.models import AIDE
+from ....project.models.AIDE.models import srm_filter_kernel
+from ....project.models.AIDE.models import utils
