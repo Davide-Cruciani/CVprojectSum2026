dfd98e2a242f9df1cbbd1d617d4031a9fae0cb49
Davide-Cruciani <davidecruciani0601@gmail.com>
2026-06-28 18:24:58 +0200

    added WongCNN

---
+++ models/CNNDetection/__init__.py
@@ -0,0 +1,6 @@
+from ...project.models.CNNDetection import networks
+from ...project.models.CNNDetection import options
+from ...project.models.CNNDetection import validate
+from ...project.models.CNNDetection import util
+from ...project.models.CNNDetection import eval
+from ...project.models.CNNDetection import eval_config
